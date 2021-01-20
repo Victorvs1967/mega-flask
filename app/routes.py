ba1e306d2e21af57a378127ec59a7efdbec41ce6
Victors1967 <victorsmirnov@me.com>
2021-01-20 18:25:16 +0200

    add login information

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, redirect, flash
 
 from app import app
+from .forms import LoginForm
 
 
 @app.route('/')
@@ -27,3 +28,13 @@ def index():
         }
     ]
     return render_template('index.html', title=title, user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    title = 'Sign In'
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data} remember_me={form.remember_me.data}')
+        return redirect('/index')
+    return render_template('login.html', title=title, form=form)
+
