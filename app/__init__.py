3631b5c715e462d0f71b45910f8f3a9ad5a85b35
Victors1967 <victorsmirnov@me.com>
2021-01-24 00:06:56 +0200

    add bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,13 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+bootstrap = Bootstrap(app)
 mail = Mail(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
